7874c2085f9c3b33a63615cbbbd5ea75ec087e85
harvey magana <heyharv2012@gmail.com>
2020-06-13 09:55:50 -0700

    Completed Task 2 of Closures.

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,20 @@ myFunction();
 
 // Explanation: 
 
+/* The nestedFunction has the ability to access the internal varialbe because is inside the scope of the myFunction function and 
+can inherit from its containing function. */
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (let i = num - 1; i >= 1; i--) {
+    num = num + i; 
+  }
+    return num; 
+}
+
+console.log(summation(4));
